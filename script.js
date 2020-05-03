86fa70cc787678b7d3cf197185be978c9d9cc7a9
Kunal <singhkunaldeep@gmail.com>
2020-05-03 13:43:34 +0530

    Final commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const navAnimation = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.4s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
+    });
+
+    // Burger Animation
+    burger.classList.toggle("toggle");
+  });
+};
+
+navAnimation();
